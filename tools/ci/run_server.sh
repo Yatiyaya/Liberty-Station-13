d75ed0d4c1f195874792113784be98d2fafb211e
Trilbyspaceclone <30435998+Trilbyspaceclone@users.noreply.github.com>
2023-03-27 15:40:40 -0400

    Update run_server.sh

--- tools/ci/run_server.sh
+++ tools/ci/run_server.sh
@@ -8,6 +8,6 @@ tools/deploy.sh ci_test
 cp tools/ci/ci_config.txt ci_test/config/config.txt
 
 cd ci_test
-DreamDaemon liberty-Landing.dmb -close -trusted -verbose -params "log-directory=ci"
+DreamDaemon liberty-station.dmb -close -trusted -verbose -params "log-directory=ci"
 cd ..
 cat ci_test/data/logs/ci/clean_run.lk
